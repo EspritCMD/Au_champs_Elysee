954992babd5b79785fd8cd16238ef6e00dad7a83
gdahboy <iphonegdah@yahoo.fr>
2018-04-21 11:28:07 +0100

    change text for use phpstorm commit

--- blog2_backend/views/postamodifier.php
+++ blog2_backend/views/postamodifier.php
@@ -280,7 +280,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			<!--inner block end here-->
 			<!--copy rights start here-->
 			<div class="copyrights">
-				<p>© 2016 Shoppy. All Rights Reserved | Design by  <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
+				<p>© 2016 Shoppy. All Rights Reserved Abderrahim Gdah | Design by  <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
 			</div>
 			<!--COPY rights end here-->
 		</div>
